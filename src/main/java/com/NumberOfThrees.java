922262d1f9a2a003a73dd61ce8df97999a0e2464
Екатерина <krasnovaEA95@yandex.ru>
2019-04-27 23:48:12 -0400

    add NumberOfThrees

--- src/main/java/com/NumberOfThrees.java
+++ src/main/java/com/NumberOfThrees.java
@@ -1,4 +1,20 @@
 package com;
 
 public class NumberOfThrees {
+    public static void main(String[] args) {
+        int a = (int) (Math.random()*900) + 99;
+        System.out.println(a);
+
+        int hundreds = a/100;
+        int tens = (a-(hundreds*100))/10;
+        int units = a - (hundreds*100) - (tens*10);
+
+        if (hundreds > tens && hundreds > units){
+            System.out.println("В числе " + a + " наибольшая цифра " + hundreds);
+        } else if (tens > hundreds && tens > units){
+            System.out.println("В числе " + a + " наибольшая цифра " + tens);
+        } else if (units > hundreds && units > tens){
+            System.out.println("В числе " + a + " наибольшая цифра " + units);
+        }
+    }
 }
