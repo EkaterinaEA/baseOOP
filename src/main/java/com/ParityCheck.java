e24ca2fca43714458e461a0cf9433033b64333ae
Екатерина <krasnovaEA95@yandex.ru>
2019-04-27 23:50:57 -0400

    add ParityCheck

--- src/main/java/com/ParityCheck.java
+++ src/main/java/com/ParityCheck.java
@@ -1,4 +1,15 @@
 package com;
 
+import java.util.Scanner;
+
 public class ParityCheck {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        n = sc.nextInt();
+        if (n%2 == 0){
+            System.out.println("Число " + n + " - чётное");
+        } else System.out.println("Число " + n + " - нечётное");
+    }
 }
