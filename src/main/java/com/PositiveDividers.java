ab498284f416cfa261a446d74250a836af99eb84
Екатерина <krasnovaEA95@yandex.ru>
2019-04-27 23:53:03 -0400

    add PositiveDividers

--- src/main/java/com/PositiveDividers.java
+++ src/main/java/com/PositiveDividers.java
@@ -1,4 +1,17 @@
 package com;
 
+import java.util.Scanner;
+
 public class PositiveDividers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите положительное натуральное число: ");
+        int n = sc.nextInt();
+        for (int i=1; i<=n/2; i++){
+            if (n%i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
+
