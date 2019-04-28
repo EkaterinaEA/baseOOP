08f15683dbf3db76242548d0d08daee868b72e00
Екатерина <krasnovaEA95@yandex.ru>
2019-04-27 23:56:47 -0400

    add QuadraticEquation

--- src/main/java/com/QuadraticEquation.java
+++ src/main/java/com/QuadraticEquation.java
@@ -1,4 +1,34 @@
 package com;
 
+import java.util.Scanner;
+
 public class QuadraticEquation {
+    public static void main(String[] args) {
+        double a;
+        double b;
+        double c;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a:");
+        a = sc.nextDouble();
+        System.out.println("Enter b:");
+        b = sc.nextDouble();
+        System.out.println("Enter c:");
+        c = sc.nextDouble();
+
+        double d = Math.pow(b,2) - 4*c*a;
+
+        double x1 = (-b + Math.sqrt(d))/(2*a);
+        double x2 = (-b - Math.sqrt(d))/(2*a);
+
+        if (d < 0) {
+            System.out.println("The equation has no solutions");
+        }
+        if (d == 0) {
+            System.out.println("Equation a*x*x + b*x + c = 0 solution: " + x1);
+        }
+        if (d > 0){
+            System.out.println("Equation a*x*x + b*x + c = 0 solutions: " + x1 + ", " + x2);
+        }
+    }
 }
