66cd37e2cd8e8b5d653f67b836a972154312f138
Екатерина <krasnovaEA95@yandex.ru>
2019-04-27 23:43:48 -0400

    add Interval

--- src/main/java/com/Interval.java
+++ src/main/java/com/Interval.java
@@ -1,4 +1,12 @@
 package com;
 
 public class Interval {
+    public static void main(String[] args) {
+        int a = (int) (Math.random()*150) + 5;
+        System.out.println(a);
+
+        if (25<a && a<100){
+            System.out.println("Число " + a + " содержится в интервале (25,100)");
+        } else System.out.println("Число " + a + " не содержится в интервале (25,100)");
+    }
 }
