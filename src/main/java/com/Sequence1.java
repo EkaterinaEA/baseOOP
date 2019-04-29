13356f6af97783e9ae7af66de771e88c56ff88c0
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:11:39 -0400

    add Sequence1

--- src/main/java/com/Sequence1.java
+++ src/main/java/com/Sequence1.java
@@ -1,4 +1,9 @@
 package com;
 
 public class Sequence1 {
+    public static void main(String[] args) {
+        for (int i = 1000; i<10000; i=i+3){
+            System.out.print(i + " ");
+        }
+    }
 }
