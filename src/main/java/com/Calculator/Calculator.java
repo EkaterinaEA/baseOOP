ed59770bfa8e5453f82d6bda173edd03357c9034
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:16:14 -0400

    add Calculator

--- src/main/java/com/Calculator/Calculator.java
+++ src/main/java/com/Calculator/Calculator.java
@@ -1,4 +1,38 @@
 package com.Calculator;
 
+
 public class Calculator {
+
+    public int subtraction(int a, int b){
+        return a - b;
+    }
+
+    public double division(int a, int b){
+        return a/b;
+    }
+
+    public int multiplication(int a, int b){
+        return a*b;
+    }
+
+    public double exponent(int a, int b){
+        return Math.pow(a, b);
+    }
+
+    public int factorial(int a){
+        int count = 1;
+        for (int i =1; i<=a; i++){
+            count = count*i;
+        }
+        return count;
+    }
+
+    public double remainderDivision(int a, int b){
+        return a%b;
+    }
+
+    public int module(int a){
+        return Math.abs(a);
+    }
+
 }
