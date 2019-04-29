6050e10b4d9fe8278de94ecde6329e58369e5a61
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:09:26 -0400

    add RoundingNumbers

--- src/main/java/com/RoundingNumbers.java
+++ src/main/java/com/RoundingNumbers.java
@@ -1,4 +1,15 @@
 package com;
 
+import java.util.Scanner;
+
 public class RoundingNumbers {
+    public static void main(String[] args) {
+        double a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        a = sc.nextDouble();
+        int roundingA = (int) Math.round(a);
+        System.out.println("Number " + a + " after rounding: " + roundingA);
+
+    }
 }
