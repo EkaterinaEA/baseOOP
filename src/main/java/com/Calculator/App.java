24a727df2845044a083ca4318b748cbd8aed328e
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:17:55 -0400

    add App

--- src/main/java/com/Calculator/App.java
+++ src/main/java/com/Calculator/App.java
@@ -1,4 +1,15 @@
 package com.Calculator;
 
 public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.subtraction(4, 5));
+        System.out.println(calculator.division(20, 5));
+        System.out.println(calculator.multiplication(5, 9));
+        System.out.println(calculator.exponent(5, 3));
+        System.out.println(calculator.factorial(4));
+        System.out.println(calculator.remainderDivision(58, 9));
+        System.out.println(calculator.module(-8));
+
+    }
 }
