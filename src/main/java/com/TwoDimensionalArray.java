e50b4d4d007de7b73e3ab210c331d6baed9db428
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:23:58 -0400

    add TwoDimensionalArray

--- src/main/java/com/TwoDimensionalArray.java
+++ src/main/java/com/TwoDimensionalArray.java
@@ -1,4 +1,20 @@
 package com;
 
 public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        int [][] array = new int[8][5];
+        for(int i=0; i<array.length; i++){
+            for (int j=0; j<array[0].length; j++){
+                array[i][j]=(int)((Math.random())*89 + 10);
+            }
+        }
+
+        for(int i=0; i<array.length; i++){
+            for (int j=0; j<array[0].length; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
