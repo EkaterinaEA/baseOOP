6c505697e07f24c0829a923d4f8e88762cb44ea4
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:20:55 -0400

    add Monitor

--- src/main/java/com/Laptop/Monitor.java
+++ src/main/java/com/Laptop/Monitor.java
@@ -1,4 +1,52 @@
 package com.Laptop;
 
+
 public class Monitor {
+
+    public String matrixType;
+    public double diagonal;
+    public String manufacturer;
+
+
+    public Monitor(String matrixType, double diagonal, String manufacturer) {
+        this.matrixType = matrixType;
+        this.diagonal = diagonal;
+        this.manufacturer = manufacturer;
+    }
+
+    public Monitor() {
+    }
+
+    public String getMatrixType() {
+        return matrixType;
+    }
+
+    public void setMatrixType(String matrixType) {
+        this.matrixType = matrixType;
+    }
+
+    public double getDiagonal() {
+        return diagonal;
+    }
+
+    public void setDiagonal(double diagonal) {
+        this.diagonal = diagonal;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "Monitor{" +
+                "matrixType='" + matrixType + '\'' +
+                ", diagonal=" + diagonal +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
+    }
 }
