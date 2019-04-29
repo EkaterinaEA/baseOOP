89adfed8d66f939144214bc55faccdf39ab8e8f3
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:22:13 -0400

    add App

--- src/main/java/com/Laptop/App.java
+++ src/main/java/com/Laptop/App.java
@@ -1,4 +1,18 @@
 package com.Laptop;
 
 public class App {
+    public static void main(String[] args) {
+        Laptop laptopArray[] = new Laptop[]{
+                new Laptop (3, 4, 9, true, new Monitor("ips", 5, "Lenovo")),
+                new Laptop(3, 4, 9, true, new Monitor("ips", 5, "Lenovo")),
+                new Laptop(3, 4, 9, true, new Monitor("ips", 5, "Lenovo")),
+                new Laptop(3, 4, 9, true, new Monitor("ips", 5, "Lenovo")),
+                new Laptop(3, 4, 9, true, new Monitor("ips", 5, "Lenovo"))
+        };
+
+        for (int i=0; i<laptopArray.length; i++){
+            System.out.println(laptopArray[i].toString());
+        }
+    }
 }
+
