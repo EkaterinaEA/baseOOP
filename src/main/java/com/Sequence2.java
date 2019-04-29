013fc0b11a80b76f4b80ef86d76c7eadbd21ed13
Екатерина <krasnovaEA95@yandex.ru>
2019-04-28 23:13:31 -0400

    add Sequence2

--- src/main/java/com/Sequence2.java
+++ src/main/java/com/Sequence2.java
@@ -1,4 +1,12 @@
 package com;
 
 public class Sequence2 {
+    public static void main(String[] args) {
+        int array[] = new int[20];
+        int a = 2;
+        for (int i=1; i<=array.length; i++){
+            System.out.print(a + " ");
+            a = a*2;
+        }
+    }
 }
