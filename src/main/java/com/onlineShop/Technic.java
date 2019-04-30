daeb013ab16fe95208d37eb13de0645f53e5384a
Екатерина <krasnovaEA95@yandex.ru>
2019-04-29 23:47:42 -0400

    add Technic

--- src/main/java/com/onlineShop/Technic.java
+++ src/main/java/com/onlineShop/Technic.java
@@ -1,4 +1,27 @@
 package com.onlineShop;
 
 public class Technic {
+
+    public double price;
+    public String color;
+    public String brand;
+
+    public Technic(double price, String color, String brand) {
+        this.price = price;
+        this.color = color;
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Technic{" +
+                "price=" + price +
+                ", color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
