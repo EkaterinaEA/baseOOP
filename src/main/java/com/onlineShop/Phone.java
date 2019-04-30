e31f4ab76bc0f3f179c2425454a33fd5dab4316e
Екатерина <krasnovaEA95@yandex.ru>
2019-04-29 23:54:38 -0400

    add Phone

--- src/main/java/com/onlineShop/Phone.java
+++ src/main/java/com/onlineShop/Phone.java
@@ -1,4 +1,21 @@
 package com.onlineShop;
 
-public class Phone {
+public class Phone extends Technic {
+
+    public String os;
+
+    public Phone(double price, String color, String brand, String os) {
+        super(price, color, brand);
+        this.os = os;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "os='" + os + '\'' +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
 }
