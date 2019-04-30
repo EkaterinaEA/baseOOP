9aa03683ebe736699b0cf0664dd76d338212b5e9
Екатерина <krasnovaEA95@yandex.ru>
2019-04-29 23:53:07 -0400

    add TV

--- src/main/java/com/onlineShop/TV.java
+++ src/main/java/com/onlineShop/TV.java
@@ -1,4 +1,21 @@
 package com.onlineShop;
 
-public class TV {
+public class TV extends Technic {
+
+    public double diagonal;
+
+    public TV(double price, String color, String brand, double diagonal) {
+        super(price, color, brand);
+        this.diagonal = diagonal;
+    }
+
+    @Override
+    public String toString() {
+        return "TV{" +
+                "diagonal=" + diagonal +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
 }
