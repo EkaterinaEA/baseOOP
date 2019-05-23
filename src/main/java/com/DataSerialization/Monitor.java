b090611874afe5cfbfc6c252928662b7c4af6e98
Екатерина <krasnovaEA95@yandex.ru>
2019-05-23 17:27:07 -0400

    add Monitor

--- src/main/java/com/DataSerialization/Monitor.java
+++ src/main/java/com/DataSerialization/Monitor.java
@@ -1,4 +1,29 @@
 package com.DataSerialization;
 
 public class Monitor {
+
+    private String name;
+    private String model;
+    private int price;
+
+    public Monitor(String name, String model, int price) {
+        this.name = name;
+        this.model = model;
+        this.price = price;
+    }
+
+    public Monitor() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
