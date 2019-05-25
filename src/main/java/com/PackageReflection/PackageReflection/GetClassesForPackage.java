0d6aa1eb6942afb87bea2555bf1d2ee175c172f7
Екатерина <krasnovaEA95@yandex.ru>
2019-05-24 22:57:12 -0400

    add GetClassesForPackage

---
+++ src/main/java/com/PackageReflection/PackageReflection/GetClassesForPackage.java
@@ -0,0 +1,96 @@
+package com.PackageReflection.PackageReflection;
+
+
+import sun.net.www.protocol.file.FileURLConnection;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+public class GetClassesForPackage {
+    private static void checkDirectory(File directory, String pckgname,
+                                       ArrayList<Class<?>> classes) throws ClassNotFoundException {
+        File tmpDirectory;
+
+        if (directory.exists() && directory.isDirectory()) {
+            final String[] files = directory.list();
+
+            //       if (files == null){
+            //           System.out.println("files == null");
+            //       }
+            for (final String file : files) {
+                if (file.endsWith(".java")) {
+                    try {
+                        classes.add(Class.forName(pckgname + '.'
+                                + file.substring(0, file.length() - 5)));
+                    } catch (final NoClassDefFoundError e) {
+                        // do nothing. this class hasn't been found by the
+                        // loader, and we don't care.
+                    }
+                } else if ((tmpDirectory = new File(directory, file))
+                        .isDirectory()) {
+                    checkDirectory(tmpDirectory, pckgname + "." + file, classes);
+                }
+            }
+        }
+    }
+
+
+    public static ArrayList<Class<?>> getClassesForPackage(String pckgname)
+            throws ClassNotFoundException {
+        final ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+
+        try {
+            final ClassLoader cld = Thread.currentThread()
+                    .getContextClassLoader();
+
+            if (cld == null)
+                throw new ClassNotFoundException("Can't get class loader.");
+
+            final Enumeration<URL> resources = cld.getResources(pckgname
+                    .replace('.', '/'));
+            URLConnection connection;
+
+            for (URL url = null; resources.hasMoreElements()
+                    && ((url = resources.nextElement()) != null);) {
+                try {
+                    connection = url.openConnection();
+
+                    if (connection instanceof FileURLConnection) {
+                        try {
+                            checkDirectory(
+                                    new File(URLDecoder.decode(url.getPath(),
+                                            "UTF-8")), pckgname, classes);
+                        } catch (final UnsupportedEncodingException ex) {
+                            throw new ClassNotFoundException(pckgname + " does not appear to be a valid package (Unsupported encoding)", ex);
+                        }
+                    } else
+                        throw new ClassNotFoundException(pckgname + " ("
+                                + url.getPath()
+                                + ") does not appear to be a valid package");
+                } catch (final IOException ioex) {
+                    throw new ClassNotFoundException(
+                            "IOException was thrown when trying to get all resources for "
+                                    + pckgname, ioex);
+                }
+            }
+        } catch (final NullPointerException ex) {
+            throw new ClassNotFoundException(
+                    pckgname
+                            + " does not appear to be a valid package (Null pointer exception)",
+                    ex);
+        } catch (final IOException ioex) {
+            throw new ClassNotFoundException(
+                    "IOException was thrown when trying to get all resources for "
+                            + pckgname, ioex);
+        }
+
+        System.out.println(classes.toString());
+        return classes;
+    }
+}
